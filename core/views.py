12996c363d2f672120bc8f113e203c1f9d0dddae
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-04-05 21:44:52 +0900

    Project images added and index developed

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from item.models import CategoryModel,Item
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold = False)[0:6]
+    category = CategoryModel.objects.all()
+    return render(request, 'core/index.html',{
+        'categories':category,
+        'items':items,
+    })
 
 def contact(request):
     return render(request,'core/contact.html')
