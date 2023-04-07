846fb7579944dd772c5f058ce56276a974fb0d39
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-04-08 03:20:53 +0900

    items detail and urls

--- emarket/urls.py
+++ emarket/urls.py
@@ -19,10 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
-app_name = 'core'
 
 urlpatterns = [
     path('core/', include('core.urls')),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
     
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
